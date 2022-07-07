3189462c09b5b61194fe19807cdcc4eec238a3f9
이규빈 <gblee12@chainlightning.kr>
2022-07-07 18:34:53 +0900

    feat: sequence hash & setdefalut

--- sequence.py
+++ sequence.py
@@ -134,3 +134,42 @@ print('sort -', f_list.sort(), '원본 수정됨', f_list)
 # List vs Array 적합한 사용성 설명
 # 리스트 기반 : 융통성, 다양한 자료형, 범용적 사용
 # 숫자 기반: ai 기계학습, 고속연산, 배열(리스트와 거의 호환)
+
+# 해시 테이블
+# 해시 값이 중복될떄 처리를 어떻게 하는가?
+# key에 value를 저장하는 구조 __dict__
+# 파이썬 dict = 해쉬테이블
+# 키 값의 연산 결과에 따라 직접 접근이 가능한 구조
+# key 값을 해싱 함수 => 해쉬 주소 -> key에 대한 value 참조
+
+# Dict 구조
+# print(__builtins__.__dict__)
+
+# Hash 값 확인
+t1 = (10, 20, (30, 40, 50))
+print(hash(t1)) # 튜플은 해쉬값이 추출 가능하다.
+
+# Dict SetDefalut 예제
+source = (('k1', 'val1')
+          ,('k1', 'val2')
+           ,('k2', 'val3')
+           ,('k2', 'val4')
+           ,('k2', 'val5'))
+
+new_dict1 = {}
+new_dict2 = {}
+
+# no use Setdefault
+for k, v in source:
+    if k in new_dict1:
+        new_dict1[k].append(v)
+    else:
+        new_dict1[k] = [v]
+
+print(new_dict1)
+
+# use Setdefault
+for k, v in source:
+    new_dict2.setdefault(k, []).append(v)
+
+print(new_dict2)
